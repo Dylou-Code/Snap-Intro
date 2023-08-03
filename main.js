b1be225a0d5b14f20c0c8f782119877a8267d684
Dylou-Code <81188980+Dylou-Code@users.noreply.github.com>
2023-08-03 16:23:49 +0200

    Feat: animation slide page, animation mobile navbar

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const primaryHeader = document.querySelector(".header");
 const navToggle = document.querySelector(".navigation__mobile-toggle");
 const primaryNav = document.querySelector(".navigation");
+const hamburgerIcon = document.getElementById("burgerIcon");
+const closeIcon = document.getElementById("burgerClose");
+const listNavigation = document.querySelectorAll(".navigation__dropdown-title");
+const dropdownList = document.querySelectorAll(".navigation__dropdown-list");
 
+/*listNavigation.addEventListener('click', () => {
+    dropdownList.classList.toggle("hidden")
+});*/
 //ajout d'un écouteur au click pour navToggle
 
 navToggle.addEventListener('click', () => {
-    console.log("ici");
-    //si primary=navigation à l'attribue false au Click le passer en True pour afficher le menu et inversement
+
+
+    hamburgerIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true);
+
     //permet de changer l'attribut
     primaryNav.toggleAttribute("data-visible");
     primaryHeader.toggleAttribute("data-overlay");
 })
+listNavigation.addEventListener('click', () => {
+    dropdownList.classList.toggle("hidden")
+});
+
+
+
+
+
 
 
 /*const primaryHeader = document.querySelector(".primary-header")
