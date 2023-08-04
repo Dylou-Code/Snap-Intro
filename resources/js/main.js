9da970ec7482fb9a2193566b507fb2e353520bed
Dylou-Code <81188980+Dylou-Code@users.noreply.github.com>
2023-08-04 16:16:06 +0200

    Animation dropdown navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,15 +4,11 @@ const primaryNav = document.querySelector(".navigation");
 const hamburgerIcon = document.getElementById("burgerIcon");
 const closeIcon = document.getElementById("burgerClose");
 const listNavigation = document.querySelector(".navigation__dropdown-title");
-/*const dropdown = document.getElementById("dropdown");*/
-
-
-
+const arrowUpIcon = document.querySelector(".arrow-up");
+const arrowDownIcon = document.querySelector(".arrow-down");
 const dropdownList = document.querySelector(".navigation__dropdown-list");
 
-
 navToggle.addEventListener('click', () => {
-
     hamburgerIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
     primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true);
@@ -23,4 +19,7 @@ navToggle.addEventListener('click', () => {
 })
 listNavigation.addEventListener('click', () => {
     dropdownList.classList.toggle("hidden")
+    arrowUpIcon.classList.toggle('hidden');
+    arrowDownIcon.classList.toggle('hidden');
+    /*dropdownList.style.animation = "slide-bottom ";*/
 });
