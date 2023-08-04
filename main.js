ff0e15bedb5dec24121988bf23354acdf4aa4d93
Dylou-Code <81188980+Dylou-Code@users.noreply.github.com>
2023-08-04 15:03:33 +0200

    change color text navbar, fix: padding &  margin Tablet

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const navToggle = document.querySelector(".navigation__mobile-toggle");
 const primaryNav = document.querySelector(".navigation");
 const hamburgerIcon = document.getElementById("burgerIcon");
 const closeIcon = document.getElementById("burgerClose");
-const listNavigation = document.querySelectorAll(".navigation__dropdown-title");
+const listNavigation = document.querySelector(".navigation__dropdown-title");
+/*const dropdown = document.getElementById("dropdown");*/
+
+
+
 const dropdownList = document.querySelector(".navigation__dropdown-list");
 
-/*listNavigation.addEventListener('click', () => {
-    dropdownList.classList.toggle("hidden")
-});*/
-//ajout d'un écouteur au click pour navToggle
 
 navToggle.addEventListener('click', () => {
-
 
     hamburgerIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
@@ -25,25 +24,3 @@ navToggle.addEventListener('click', () => {
 listNavigation.addEventListener('click', () => {
     dropdownList.classList.toggle("hidden")
 });
-
-
-
-
-
-
-
-/*const primaryHeader = document.querySelector(".primary-header")
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".primary-navigation")*/
-
-//ajout d'un écouteur au click pour navToggle
-
-/*
-navToggle.addEventListener('click', () => {
-    //si primary=navigation à l'attribue false au Click le passer en True pour afficher le menu et inversement
-    primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true);
-    //permet de changer l'attribut
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
-})
-*/
